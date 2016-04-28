9087786e090f6d03f495ab058289452c0dd84428
Смирнов Игорь Владимирович <SmirnovIV@eka.tmk.group>
2016-04-28 16:37:08 +0500

    Developing Kalman SLAM test

--- kalman.cpp
+++ kalman.cpp
@@ -2,10 +2,9 @@
 #include <gtest/gtest.h>
 #include <cstdlib>
 
-// СВ, равномерно от -1 до 1
-double rnd()
+double rnd(double min, double max)
 {
-	return ((double) rand() / RAND_MAX) * 2.0 - 1.0;
+	return ((double) rand() / RAND_MAX) * (max - min) + min;
 }
 
 //Подпрограмма нормального распределения - возв СВ по гауссу с МО 0 и дисп 1.                
@@ -16,7 +15,7 @@ double rand_norm()
 	if(flag) { flag = false; return(r);	}
 	while(1)
 	{
-		double v1 = rnd(), v2 = rnd(), s = v1 * v1 + v2 * v2;
+		double v1 = rnd(-1, 1), v2 = rnd(-1, 1), s = v1 * v1 + v2 * v2;
 		if(s <= 1.0 && s > 0.0)
 		{
 			r = v1 * sqrt(-2.0 * log(s) / s); //за один раз получаем 2 СВ
@@ -31,10 +30,10 @@ TEST(KalmanTest, gauss_mul)
 	srand(5);
 	for (int x = 0; x < 100; x++)
 	{
-		double u1 = ((double)rand() / RAND_MAX) * 20.0 - 10.0; // Матожидания
-		double u2 = ((double)rand() / RAND_MAX) * 20.0 - 10.0;
-		double s1 = ((double)rand() / RAND_MAX) * 5.0 + 0.1; // Дисперсии
-		double s2 = ((double)rand() / RAND_MAX) * 5.0 + 0.1;
+		double u1 = rnd(-10, 10); // Матожидания
+		double u2 = rnd(-10, 10);
+		double s1 = rnd(0.01, 10); // Дисперсии
+		double s2 = rnd(0.01, 10);
 
 		Kalman<double, 1> k;
 		EXPECT_DOUBLE_EQ(k.Sigma_(0, 0), 0.0);
@@ -54,6 +53,43 @@ TEST(KalmanTest, gauss_mul)
 		EXPECT_NEAR(k.Sigma_(0, 0),            s1 * s2 / (s1 + s2), 1E-10); // Произведение нормальных распределений
 		EXPECT_NEAR(k.X_(0),       (u1 * s2 + u2 * s1) / (s1 + s2), 1E-10);
 	}
+}
+
+TEST(KalmanTest, slam1d)
+{
+	Kalman<double, 4> k;
+	k.X_ << 0.0, 1.0, rnd(0, 10), rnd(0, 10); // Позиция, скорость, метка1, метка2
+	double dt = 0.1;
+	k.G_(1, 0) = dt; // позиция = позиция + скорость * dt
+	k.Sigma_(0, 0) = 0.1;
+	k.Sigma_(1, 1) = 0.1;
+	k.Sigma_(2, 2) = 100; // Мы не знаем, где метка
+	k.Sigma_(3, 3) = 100; // Мы не знаем, где метка
+	k.Q_(0, 0) = 1; // 
+	k.Q_(1, 1) = 1; // 
+	k.Q_(2, 2) = 0; // Метка не двигается
+
+	Corrector<double, 4, 2> c; // Датчик определяет смещение относительно метки
+	c.H_ << -1.0, 0.0, 1.0, 0.0,
+			-1.0, 0.0, 0.0, 1.0;
+	c.R_(0, 0) = 0.1;
+	c.R_(1, 1) = 0.1;
+
+	Eigen::Matrix<double, 2, 1> z;
+	double mark1 = 10;// rnd(0, 10); // Реальная позиция метки
+	double mark2 = 20;// rnd(0, 10); // Реальная позиция метки
+	
+	for (double t = 0; t < 1; t += dt)
+	{
+		z(0) = mark1 - k.X_(0);
+		z(1) = mark2 - k.X_(1);
+		std::cout << "X =" << std::endl << k.X_ << std::endl;
+		std::cout << "Sigma =" << std::endl << k.Sigma_ << std::endl;
+		k.predict();
+		c.correct(k.X_, k.Sigma_, z);
+
+	}
+
 }
 
 
