09fe8f7251d85b1218e06f00c1c48781e168efcb
Смирнов Игорь Владимирович <SmirnovIV@eka.tmk.group>
2016-05-06 15:39:13 +0500

    Add NormalDistribution ellipse with tests

--- normdist.cpp
+++ normdist.cpp
@@ -1,6 +1,16 @@
 #include "normdist.h"
 #include <gtest/gtest.h>
 
+bool NDEllipse::test(double x, double y) const
+{
+	x -= cx;
+	y -= cy;
+	double a = cos(alpha), b = sin(alpha);
+	double x1 = a * x + b * y;
+	double y1 = a * y - b * x;
+	return x1 * x1 / (rx * rx) + y1 * y1 / (ry * ry) < 1.0;
+}
+
 TEST(NormalDistributionTest, analize)
 {
 	std::vector<Eigen::Vector2d> v;
@@ -24,7 +34,7 @@ double rnd(double min, double max)
 	return ((double)rand() / RAND_MAX) * (max - min) + min;
 }
 
-//Подпрограмма нормального распределения - возв СВ по гауссу с МО 0 и дисп 1.                
+//РџРѕРґРїСЂРѕРіСЂР°РјРјР° РЅРѕСЂРјР°Р»СЊРЅРѕРіРѕ СЂР°СЃРїСЂРµРґРµР»РµРЅРёСЏ - РІРѕР·РІ РЎР’ РїРѕ РіР°СѓСЃСЃСѓ СЃ РњРћ 0 Рё РґРёСЃРї 1.                
 double rand_norm()
 {
 	static double r;
@@ -35,7 +45,7 @@ double rand_norm()
 		double v1 = rnd(-1, 1), v2 = rnd(-1, 1), s = v1 * v1 + v2 * v2;
 		if (s <= 1.0 && s > 0.0)
 		{
-			r = v1 * sqrt(-2.0 * log(s) / s); //за один раз получаем 2 СВ
+			r = v1 * sqrt(-2.0 * log(s) / s); //Р·Р° РѕРґРёРЅ СЂР°Р· РїРѕР»СѓС‡Р°РµРј 2 РЎР’
 			flag = 1;
 			return v2 * sqrt(-2.0 * log(s) / s);
 		}
@@ -64,4 +74,98 @@ TEST(NormalDistributionTest, generate)
 	EXPECT_NEAR(nd.sigma(0, 1), nd2.sigma(0, 1), 0.02);
 	EXPECT_NEAR(nd.sigma(1, 0), nd2.sigma(1, 0), 0.02);
 	EXPECT_NEAR(nd.sigma(1, 1), nd2.sigma(1, 1), 0.02);
+}
+
+TEST(NormalDistributionTest, ellipse_test)
+{
+	NDEllipse e;
+	e.cx = 1;
+	e.cy = 2;
+	e.rx = 1;
+	e.ry = 2;
+	e.alpha = -0.2;
+	EXPECT_TRUE(e.test(1, 2));
+	EXPECT_FALSE(e.test(1, 0));
+	EXPECT_TRUE (e.test(0, 2));
+	EXPECT_FALSE(e.test(0, 0));
+	EXPECT_FALSE(e.test(0, 3));
+}
+
+const double PI = 3.1415926535897932384626433832795;
+
+TEST(NormalDistributionTest, alpha)
+{
+	Eigen::Matrix2d s;
+	Eigen::Vector2d m;
+	m << 0, 0;
+	s << 4.0, 0.0,
+		 0.0, 1.0;
+	NormalDistribution<double, 2> nd(m, s);
+	EXPECT_NEAR(nd.ellipse(1).alpha, 0.0, 1E-6);
+	EXPECT_NEAR(nd.ellipse(1).rx,    2.0, 1E-6);
+	EXPECT_NEAR(nd.ellipse(1).ry,    1.0, 1E-6);
+	nd.sigma << 
+		1.0, 0.0,
+		0.0, 2.0;
+	EXPECT_NEAR(nd.ellipse(1).alpha, PI / 2, 1E-6);
+	EXPECT_NEAR(nd.ellipse(1).rx, sqrt(2.0), 1E-6);
+	EXPECT_NEAR(nd.ellipse(1).ry,       1.0, 1E-6);
+	nd.sigma <<
+		1.0, 0.0,
+		0.0, 1.0;
+	EXPECT_NEAR(nd.ellipse(1).alpha, 0.0, 1E-6);
+	EXPECT_NEAR(nd.ellipse(1).rx,	 1.0, 1E-6);
+	EXPECT_NEAR(nd.ellipse(1).ry,    1.0, 1E-6);
+	nd.sigma << 
+		1.0, 0.5,
+		0.5, 1.0;
+	EXPECT_NEAR(nd.ellipse(1).alpha, PI / 4, 1E-6);
+	nd.sigma <<
+		1.0,-0.5,
+	   -0.5, 1.0;
+	EXPECT_NEAR(nd.ellipse(1).alpha, -PI / 4, 1E-6);
+}
+
+void hitTest(double sx, double sy, double cv)
+{
+	Eigen::Matrix2d s;
+	Eigen::Vector2d m;
+	m << 0, 0;
+	s << sx, cv,
+		 cv, sy;
+	NormalDistribution<double, 2> nd(m, s);
+	NDEllipse e1 = nd.ellipse(1), e2 = nd.ellipse(2), e3 = nd.ellipse(3);
+	int count = 100000;
+	double s1 = 0, s2 = 0, s3 = 0;
+	for (int x = 0; x < count; x++)
+	{
+		Eigen::Vector2d m;
+		nd.generate(m);
+		if (e1.test(m(0), m(1))) s1++;
+		if (e2.test(m(0), m(1))) s2++;
+		if (e3.test(m(0), m(1))) s3++;
+	}
+	EXPECT_NEAR(s1 / count, 0.3935, 0.0025);
+	EXPECT_NEAR(s2 / count, 0.8647, 0.0025);
+	EXPECT_NEAR(s3 / count, 0.9889, 0.0025);
+}
+
+TEST(NormalDistributionTest, hit_1x1)
+{
+	hitTest(1, 1, 0);
+}
+
+TEST(NormalDistributionTest, hit_10x1)
+{
+	hitTest(10, 1, 0);
+}
+
+TEST(NormalDistributionTest, hit_1x10)
+{
+	hitTest(1, 10, 0);
+}
+
+TEST(NormalDistributionTest, hit_1x10c3)
+{
+	hitTest(1, 10, 3);
 }
--- normdist.h
+++ normdist.h
@@ -1,9 +1,17 @@
 #pragma once
 #include <Eigen/Core>
 #include <Eigen/Cholesky>
+#include <Eigen/LU>
 #include <vector>
 
 double rand_norm();
+
+struct NDEllipse
+{
+	double alpha, rx, ry;
+	double cx, cy;
+	bool test(double x, double y) const;
+};
 
 template<class T, int N>
 class NormalDistribution
@@ -36,14 +44,20 @@ public:
 	{
 		calcA();
 	}
-	void generate(Vector & out)
+	void generate(Vector & out) const
 	{
 		Vector t;
 		for (int x = 0; x < N; x++) t(x) = rand_norm();
 		out = A * t + mean;
 	}
-	void ellipse(double sigmaCount)
+	NDEllipse ellipse(double dev, int x = 0, int y = 1) const
 	{
-
+		Matrix inv = sigma.inverse();
+		double alpha = atan2(sigma(0, 1) + sigma(1, 0), sigma(0, 0) - sigma(1, 1)) / 2;
+		double i00 = inv(0, 0), i11 = inv(1, 1), i01 = inv(1, 0) + inv(0, 1);
+		double a = sin(alpha), b = cos(alpha);
+		double rx = dev / sqrt(i00 * b * b + i01 * a * b + i11 * a * a);
+		double ry = dev / sqrt(i00 * a * a - i01 * a * b + i11 * b * b);
+		return {alpha, rx, ry, mean(x), mean(y)};
 	}
 };
