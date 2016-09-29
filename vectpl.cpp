3f55effda3e7d7ad03b111481cbd103d8222dc72
Igor Smirnov <smirnoviv@rambler.ru>
2016-09-29 15:26:07 +0500

    Add VectorMerge and Matrix templates

--- vectpl.cpp
+++ vectpl.cpp
@@ -3,7 +3,7 @@
 
 enum Enum
 {
-    None,
+    Height,
     Attitude,
     Velocity,
     Position
@@ -13,9 +13,10 @@ enum Enum
 
 TEST(vtpVectorTest, basic)
 {
-    vtp::Vector<vtp::Tag<Velocity, double>, vtp::Tag<Position, int>> v1;
+    vtp::Vector<vtp::Tag<Velocity, double>, vtp::Tag<Position, int>, vtp::Tag<Attitude, std::string>> v1;
+    EXPECT_TRUE(v1.has<Attitude>());
     vtp::Vector<vtp::Tag<Velocity, double>, vtp::Tag<Attitude, std::string>> v2;
-    vtp::Vector<vtp::Tag<Position, int>, vtp::Tag<Attitude, std::string>> v3;
+    vtp::Vector<vtp::Tag<Position, int>, vtp::Tag<Attitude, std::string>, vtp::Tag<Velocity, double>> v3;
     vtp::get<Velocity>(v1) = 1.4;
     vtp::get<Position>(v1) = 1;
     vtp::get<Velocity>(v2) = 1.4;
@@ -23,6 +24,8 @@ TEST(vtpVectorTest, basic)
     vtp::get<Position>(v3) = 2;
     vtp::get<Attitude>(v3) = "trust";
 
+    static_assert(v2.subsetOf(v1), "error");
+    static_assert(!v1.subsetOf(v2), "error");
     v1 += v2;
     v3 += v2;
     EXPECT_EQ(vtp::get<Velocity>(v1), 2.8);
@@ -30,4 +33,39 @@ TEST(vtpVectorTest, basic)
 
     EXPECT_EQ(vtp::get<Attitude>(v3), "trusttest");
     EXPECT_EQ(vtp::get<Position>(v3), 2);
+}
+
+TEST(vtpVectorTest, merge)
+{
+    using namespace vtp;
+    typedef Vector<vtp::Tag<Velocity, double>> V1;
+    static_assert(V1::template subsetOf<V1>(), "subset error");
+    typedef Vector<vtp::Tag<Attitude, std::string>> V2;
+    static_assert(!V1::template subsetOf<V2>(), "subset error");
+    typedef VectorMerge<V1, V2> V3;
+    static_assert(V1::template subsetOf<V3>(), "subset error");
+    static_assert(V2::template subsetOf<V3>(), "subset error");
+    static_assert(!V3::template subsetOf<V1>(), "subset error");
+}
+
+TEST(vtpMatrixTest, basic)
+{
+    using namespace vtp;
+    typedef Vector<Tag<Velocity, double>> V1;
+    typedef Vector<Tag<Position, double>> V2;
+    typedef VectorMerge<V1, V2> V3;
+    V1 v1;
+    V2 v2;
+    V3 v3 = v1 + v2;
+
+
+    Matrix<
+            Tag<Velocity, Vector<Tag<Velocity, double>, Tag<Position, double>>>,
+            Tag<Position, Vector<Tag<Velocity, double>, Tag<Position, double>>>
+            > mat1;
+    Matrix<
+            Tag<Velocity, Vector<Tag<Velocity, double>, Tag<Position, double>>>,
+            Tag<Position, Vector<Tag<Velocity, double>, Tag<Position, double>>>
+            > mat2;
+    auto mat3 = mat1 * mat2;
 }
--- vectpl.h
+++ vectpl.h
@@ -1,5 +1,6 @@
 #ifndef VECTPL_H
 #define VECTPL_H
+#include <assert.h>
 
 namespace vtp {
 
@@ -27,11 +28,13 @@ public:
     template<class O>
     constexpr inline Vector<> & operator += (const O &) { return *this; }
     //constexpr bool has(Enum) const { return false; }
-    template<Enum tag> constexpr bool has() const { return false; }
+    template<Enum tag> static constexpr bool has() { return false; }
+    template<class Other> static constexpr bool subsetOf() { return true; }
     constexpr int &getItem() const { return *(int *) nullptr; }
     //template<Enum tag> constexpr get
 };
 
+//-------------------------- FindItem ------------------------------------------------------------------//
 /// Шаблон для поиска элемента вектора по метке
 template<Enum tag, class Item> struct FindItem;
 
@@ -72,6 +75,33 @@ constexpr const typename FindItem<tag, Vector<Types...>>::Item & get(const Vecto
     return (((Vector&) vector).getItem());
 }
 
+/// Класс, представяющий собой объединение двух векторов
+template<class Vector1, class Vector2>
+class VectorMerge;
+
+template<bool has, class Vector1, class Vector2>
+class VectorMergeBool;
+
+/// Если Type2 входит в Vector1 - просто убираем его
+template<class Vector1, typename Type2, typename... Types2>
+class VectorMergeBool<true, Vector1, Vector<Type2, Types2...>> : public VectorMerge<Vector1, Vector<Types2...>> { };
+
+template<class Vector, typename... Types> struct VectorPush;
+
+template<typename Type, typename... Types>
+struct VectorPush<Vector<Types...>, Type> : Vector<Type, Types...> {};
+
+/// Если Type2 не входит в Vector1 - перекладываем
+template<class Vector1, typename Type2, typename... Types2>
+class VectorMergeBool<false, Vector1, Vector<Type2, Types2...>> : public VectorMerge<VectorPush<Vector1, Type2>, Vector<Types2...>> { };
+
+template<class Vector1, typename Type2, typename... Types2>
+class VectorMerge<Vector1, Vector<Type2, Types2...>> : public VectorMergeBool<Vector1::template has<(Enum)Type2::tag>(), Vector1, Vector<Type2, Types2...>> { };
+
+template<class Vector1>
+class VectorMerge<Vector1, Vector<>> : public Vector1 { };
+
+//-------------------------- get ------------------------------------------------------------------//
 /// Функция получения значения
 template<Enum tag, class... Types> inline
 constexpr typename FindItem<tag, Vector<Types...>>::Item & get(Vector<Types...> & vector)
@@ -81,16 +111,19 @@ constexpr typename FindItem<tag, Vector<Types...>>::Item & get(Vector<Types...> 
 }
 
 /// Рекурсивная специализация вектора
-template<class Item, typename... Other>
-class Vector<Item, Other...> : private Vector<Other...>
+template<class Item, typename... Others>
+class Vector<Item, Others...> : private Vector<Others...>
 {
+    //inline
 public:
-    typedef Vector<Other...> Parent;
+    typedef Vector<Others...> Parent;
+    typedef Vector<Item, Others...> This;
     Item item;
     constexpr typename Item::Item &getItem() { return item.item; }
 
-    //constexpr bool has(Enum tag) const { return (Enum) Item::tag == tag ? true : Parent::has(tag); }
-    template<Enum tag> constexpr bool has() const { return (Enum) Item::tag == tag ? true : Parent::template has<tag>(); }
+    template<Enum tag> static constexpr bool has() { return (Enum) Item::tag == tag ? true : Parent::template has<tag>(); }
+    template<class Other> static constexpr bool subsetOf() { return Other::template has<(Enum)Item::tag>() ? Parent::template subsetOf<Other>() : false; }
+    template<class Other> static constexpr bool subsetOf(const Other &) { return This::template subsetOf<Other>(); }
     //template<Enum tag> constexpr Item &get() { return
 
     /*template<Enum tag, class... Types> inline
@@ -100,26 +133,35 @@ public:
         return (((Vector&) *this).item);
     }*/
 
-    template<class O>
-    Vector<Item, Other...> & operator += (const O & other)
+    template<class Other>
+    Vector<Item, Others...> & operator += (const Other & other)
     {
-        Vector<Other...>::operator +=(other);
+        //static_assert(Other::template subsetOf<This>(), "Unable to add");
+        Vector<Others...>::operator +=(other);
         if(other.has<(Enum)Item::tag>())
             item.item += get<Enum(Item::tag)>(other);
         return *this;
     }
+    template<class Other>
+    inline VectorMerge<This, Other> operator + (const Other & other) const
+    {
+        VectorMerge<This, Other> result;
+
+        //if()
+        return result;
+    }
 };
 
-/*struct Matrix
+template<typename... Vectors>
+class Matrix : public Vector<Vectors...>
 {
+public:
+    template<class Other>
+    Matrix<Vectors...> operator * (const Other & other) const
+    {
+
+    }
 };
-
-template<Enum A, Enum B>
-int &get(Matrix &mat) { }
-
-template<>
-int &get<Attitude, Velocity>(Matrix &mat) { }
-*/
 
 }
 
